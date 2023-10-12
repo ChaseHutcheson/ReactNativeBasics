295970a387a8fdb0e459dff21bb2b77753087579
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2023-10-12 02:19:01 -0400

    Working On UI

--- app/index.js
+++ app/index.js
@@ -3,7 +3,9 @@ import { View, Text } from "react-native";
 const Home = () => {
     return (
         <View>
-            <Text>Homy</Text>
+            <Text style={{textAlign: "center", 
+            fontWeight: "bold", margin: 20, 
+            color: "red"}}>Jesse, We need to build</Text>
         </View>
     )
 }
